b8d95b07e1131e268609a1a96c022da026d82342
Ian-Garrigan <igarrigan@hotmail.com>
2023-08-20 23:14:23 +0000

    Customize admin & fix href + typo in templates

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Display the Category model fields in the admin view
+    """
+
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Display the Product model fields in the admin view
+    """
+
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+    
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
