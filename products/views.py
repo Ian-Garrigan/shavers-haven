7185031d62b8efb706c23402f53e92122e9a7868
Ian-Garrigan <igarrigan@hotmail.com>
2023-08-21 02:59:16 +0000

    Sorting products added

--- products/views.py
+++ products/views.py
@@ -10,14 +10,30 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None 
+    sort = None
+    direction = None
+
+    # Check if sort fileter is requested by user
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
     """
     Verify the presence of the Category; if found, divide it into a list using commas,
     which will then be employed to narrow down the queryset of all products to those that correspond.
     """
 
-    if request.GET:
-         if 'category' in request.GET:
+    if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
@@ -33,12 +49,13 @@ def all_products(request):
         queries = Q(name__icontains=query) | Q(description__icontains=query)
         products = products.filter(queries)
 
-        products = products.filter(queries)    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting' : current_sorting,
     }
 
     return render(request, 'products/products.html', context)
