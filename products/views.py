8f2d706c0e5d8592202020589fe09109b95a1271
Ian-Garrigan <igarrigan@hotmail.com>
2023-08-21 01:08:00 +0000

    Added products details functionality & fix url typo

--- products/views.py
+++ products/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
-
-# Create your views here.
-from django.shortcuts import render
 
 # Create your views here.
 def all_products(request):
@@ -15,3 +12,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """A view to show details of product"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
