76d73ad121218b7bf706eab3aab880e9bbf62ed5
Ian-Garrigan <igarrigan@hotmail.com>
2023-08-22 21:55:32 +0000

    Implement stripe payment

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,51 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+// get the key and slice of quotations at the end and beginning
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+// variable from script in base.html
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+
+var card = elements.create('card', {style: style});
+
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+// mount the card element to the fieldset users pay info
+card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contents import bag_contents
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -9,10 +13,16 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ni0CtLexqXc4rppy58LEsXUAbTGdywUX1O9LykNN2YkXjnOuddmLhpLt19JCjVKt1670yyXjPgdI7nCADzayAMQ00rilpgd8A',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
