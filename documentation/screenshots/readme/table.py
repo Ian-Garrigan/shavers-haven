f51895875398ad613650eec3449b9154c518cfcb
Ian-Garrigan <igarrigan@hotmail.com>
2023-08-22 14:07:38 +0000

    Python auto print table

---
+++ documentation/screenshots/readme/table.py
@@ -0,0 +1,7 @@
+def print_issues():
+    """function to pring issues for readme in a form of table"""
+    for i in range(140):
+        print(f'|  [#{i}](https://github.com/Ian-Garrigan/shavers-haven/issues{i}) |  |')
+
+
+print_issues()
