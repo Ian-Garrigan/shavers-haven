78d175083ad641ee416021218370d5f50e76ad0c
Ian-Garrigan <igarrigan@hotmail.com>
2023-10-16 00:51:18 +0000

    Fix checkout process & create email confirmations

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -12,8 +12,6 @@ var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
-var card = elements.create('card', {style: style});
-
 var style = {
     base: {
         color: '#000',
@@ -30,6 +28,7 @@ var style = {
     }
 };
 
+var card = elements.create('card', { style: style });
 // mount the card element to the fieldset users pay info
 card.mount('#card-element');
 
@@ -51,9 +50,9 @@ card.addEventListener('change', function (event) {
 
 var form = document.getElementById('payment-form');
 
-form.addEventListener('submit', function(ev) {
+form.addEventListener('submit', function (ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true});
+    card.update({ 'disabled': true });
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
@@ -67,57 +66,56 @@ form.addEventListener('submit', function(ev) {
     };
 
     var url = '/checkout/cache_checkout_data/';
-    
-    $.post(url, postData).done(function() {
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                email: $.trim(form.email.value),
-                address:{
-                    line1: $.trim(form.address_line1.value),
-                    line2: $.trim(form.address_line2.value),
-                    city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
-                    state: $.trim(form.county.value),
+
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
                 }
-            }
-        },
-        shipping: {
+            },
+            shipping: {
                 name: $.trim(form.full_name.value),
                 phone: $.trim(form.phone_number.value),
-                address:{
-                    line1: $.trim(form.address_line1.value),
-                    line2: $.trim(form.address_line2.value),
-                    postal_code: $.trim(form.post_code.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    postal_code: $.trim(form.postcode.value),
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     state: $.trim(form.county.value),
                 }
             },
-        }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
+        }).then(function (result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
                 <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
                 </span>
                 <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100);
-    $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false });
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        }
-    });
-}).fail(function () {
-    // just reload the page, the error will be in django messages
-    location.reload();
-})
+        });
+    }).fail(function () {
+        location.reload();
+    })
 });
--- shavers_haven/settings.py
+++ shavers_haven/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = [ 'shavers-haven-b1ce6026470c.herokuapp.com', 'localhost', '8000-iangarrigan-shavershave-8q503fjgfgb.ws-eu102.gitpod.io', '8000-iangarrigan-shavershave-8q503fjgfgb.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = [ 'shavers-haven-b1ce6026470c.herokuapp.com', 'localhost', '8000-iangarrigan-shavershave-z3o1rtq39lu.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -221,14 +221,25 @@ if 'USE_AWS' in os.environ:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
-STRIPE_CURRENCY = 'eur'
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "shavershaven@example.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
+# Stripe
+STRIPE_CURRENCY = 'eur'
 # get from env to keep safe
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'shavershaven@example.com'
